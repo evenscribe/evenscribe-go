c15e971cb43a7b0bf58262669e17b362aea957a0
Sangit Manandhar <rux12@icloud.com>
2024-05-13 14:32:23 -0400

    chore: update Log struct

--- main.go
+++ main.go
@@ -5,7 +5,26 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
+
+type LogOwner struct {
+	HostName string `json:"host_name"` /* Identifier for frontend */
+	AppName  string `json:"app_name"`  /* Identifier for backend */
+}
+
+type Log struct {
+	Level   string `json:"level"`
+	Message string `json:"message"`
+}
+
+type LogEntry struct {
+	TimeStamp int64  `json:"@timestamp"`
+	Message_  string `json:"_msg"`
+
+	LogOwner LogOwner `json:"log_owner"`
+	Log      Log      `json:"log"`
+}
 
 // LogLevel represents the severity level of a log message.
 type LogLevel int
@@ -17,12 +36,6 @@ const (
 	ERROR
 	FATAL
 )
-
-// LogMessage represents a log message with a message string and a severity level.
-type LogMessage struct {
-	Message string
-	Level   LogLevel
-}
 
 // Olympus represents a client for the Olympus server daemon.
 type Olympus struct {
@@ -40,7 +53,7 @@ func New() *Olympus {
 // Start establishes a connection to the Olympus server daemon and starts a goroutine
 // to handle the connection and listen for the exit signal.
 func (o *Olympus) Start() error {
-	conn, err := net.Dial("unix", "/tmp/unixsock")
+	conn, err := net.Dial("unix", "/tmp/olympus_socket.sock")
 	if err != nil {
 		return err
 	}
@@ -69,7 +82,7 @@ func (o *Olympus) Stop() {
 }
 
 // Log sends a log message to the Olympus server daemon.
-func (o *Olympus) Log(message LogMessage) error {
+func (o *Olympus) Log(message LogEntry) error {
 	if o.conn == nil {
 		return fmt.Errorf("connection to Olympus server is not established")
 	}
@@ -95,9 +108,17 @@ func main() {
 		log.Fatalf("Failed to start Olympus client: %v", err)
 	}
 
-	message := LogMessage{
-		Message: "Hello, Olympus!",
-		Level:   INFO,
+	message := LogEntry{
+		Message_:  "Hello, Olympus!",
+		TimeStamp: time.Now().Unix(),
+		LogOwner: LogOwner{
+			HostName: "localhost",
+			AppName:  "olympus-client",
+		},
+		Log: Log{
+			Level:   "INFO",
+			Message: "Hello, Olympus!",
+		},
 	}
 
 	err = olympus.Log(message)
