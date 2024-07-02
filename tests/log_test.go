c0efec8598b39a955fa020ae77467d91ecd469e2
Vidur Khanal <vidurkhanal@gmail.com>
2024-07-01 22:58:35 -0400

    chore: benchmarks

--- tests/log_test.go
+++ tests/log_test.go
@@ -1,27 +1,44 @@
 package logs_test
 
 import (
-	"testing"
-	"time"
-
 	log "github.com/evenscribe/evenscribe-go"
+	"testing"
 )
 
-func TestBuildLog(t *testing.T) {
-	err := log.NewLogger(
-		log.NewOptions().
-			WithDefaults().
-			WithResourceAttributes(map[string]string{"evnviroment": "testing"}).
-			WithServiceName("logger_test").
-			WithPrintToStdout(false),
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
+// func IntMin(a, b int) int {
+// 	if a < b {
+// 		return a
+// 	}
+// 	return b
+// }
+//
+// func BenchmarkIntMin(b *testing.B) {
+//
+// 	for i := 0; i < b.N; i++ {
+// 		IntMin(1, 2)
+// 	}
+// }
 
-	for range 1000 {
-		// log.InfoS("Hello world", "log-attributes", map[string]string{"hello": "world"}, "trace-id", 12333, "span-id", 11111)
-		log.Info("Hello world")
-	}
-	time.Sleep(6 * time.Minute)
+func BenchmarkLoggging(b *testing.B) {
+	b.Run("Lsdds", func(b *testing.B) {
+		err := log.NewLogger(
+			log.NewOptions().
+				WithDefaults().
+				WithResourceAttributes(map[string]string{"evnviroment": "testing"}).
+				WithServiceName("logger_test").
+				WithPrintToStdout(false),
+		)
+
+		if err != nil {
+			b.Fatal(err)
+		}
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				log.InfoS("Hello world", "log-attributes", map[string]string{"hello": "world"}, "trace-id", 12333, "span-id", 11111)
+			}
+
+		})
+
+	})
 }
